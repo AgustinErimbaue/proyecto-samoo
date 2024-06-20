8bb3de54e4f8fdb9a0a7eb543410bca8d500f14d
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-06-20 12:54:54 +0200

    se importa componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Register from './component/Register/Register'
+import CompanyRegister from './component/CompanyRegister/CompanyRegister';
+
 
 function App() {
 
@@ -7,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/register'element={<Register />} />
+        <Route path="/CompanyRegister" element={<CompanyRegister />} />
       </Routes>
     </BrowserRouter>
   )
