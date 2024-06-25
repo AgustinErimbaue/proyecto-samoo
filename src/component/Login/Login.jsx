18eb1e1c7812dabae8ae4cf321f4f9043adbca0f
jaironf <jaironf.94@gmail.com>
2024-06-25 20:52:27 +0200

    Estilo dado a login

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import { login, reset } from '../../features/auth/authSlice'
 import { useDispatch, useSelector } from 'react-redux'
+import { Box, Button, FormControl, Heading, Input, Text } from '@chakra-ui/react'
+import './Login.scss'
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -45,19 +47,43 @@ const Login = () => {
   }
 
   return (
-    <div className="form-box">
+    <Box className='body-login' p='4' borderRadius='md'>
+    <Box className="form-box" p="4" borderWidth="1px" borderRadius="md" boxShadow="md">
     <form className="form" onSubmit={onSubmit}>
-        <span className="title">Login</span>
-        <span className="subtitle">Welcome back.</span>
-        <div className="form-container">
-          <input type="email" name='email' className="input" placeholder="Email" value={email} onChange={onChange}/>
-          <input type="password" name='password' className="input" placeholder="Password" value={password} onChange={onChange}/>
-        </div>
-        <button>Login</button>
+      <Heading as="span" size="lg" mb="4" display="block">Inicia sesión</Heading>
+      <Box className="form-container" mb="4">
+        <FormControl mb="4">
+          <Text>Email</Text>
+          <Input 
+            type="email" 
+            name="email" 
+            placeholder="example@youremail.com" 
+            value={email} 
+            onChange={onChange} 
+          />
+        </FormControl>
+        <FormControl mb="4">
+          <Text>Contraseña</Text>
+          <Input 
+            type="password" 
+            name="password" 
+            placeholder="******************" 
+            value={password} 
+            onChange={onChange} 
+          />
+        </FormControl>
+        <FormControl>
+          <Text>Número de participantes</Text>
+          <Input placeholder='aquí deberia ir un contador de asistentes'/>
+        </FormControl>
+      </Box>
+      <Button type="submit" colorScheme="blue" w="full" className='login-btn'>Enviar</Button>
     </form>
-    <div className="form-section">
-    </div>
-    </div>
+    <Box className="form-section" mt="4">
+     <Text className='text-register'>Regístrate aquí <Link to='/register' className='link-register'> Registrarse</Link> </Text>
+    </Box>
+  </Box>
+  </Box>
   )
 }
 
