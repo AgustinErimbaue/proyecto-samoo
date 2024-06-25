1409efa6755eb9f9a0fd84c552bd1c16c23b9c96
jaironf <jaironf.94@gmail.com>
2024-06-25 13:38:05 +0200

    Estructura de la pantalla de participantes terminada

--- src/component/Participants/Participants.jsx
+++ src/component/Participants/Participants.jsx
@@ -1,31 +1,32 @@
 import React from 'react'
 import './Participants.scss'
+import { Avatar, AvatarGroup, Box, Button, Heading, Text, Image } from '@chakra-ui/react'
 
 
 
 
 const Participants = () => {
     return (
-        <div className='participants-body'>
+        <Box className="participants-body" padding="20px">
             <header>
-                <h1 className='title-page'>Participantes</h1>
-                <div className='btn-filters'>
-                    <button>Ponentes</button>
-                    <button>Empresas</button>
-                    <button>Asistentes</button>
-                    <button>Platinum</button>
-                    <button>Gold</button>
-                    <button>Silver</button>
-                </div>
+                <Heading className="title-page" as="h1" size="xl" mb="4">Participantes</Heading>
+                <Box className="btn-filters" display="flex" flexWrap="wrap" gap="10px" mb="4" mt='50px'>
+                    <Button size="md">Ponentes</Button>
+                    <Button size="md">Empresas</Button>
+                    <Button size="md">Asistentes</Button>
+                    <Button size="md">Platinum</Button>
+                    <Button size="md">Gold</Button>
+                    <Button size="md">Silver</Button>
+                </Box>
             </header>
-            <div className='suppliers-header'>
-                <h2>Empresas</h2>
-                <div className='show-data'>
-                    <button>Ver datos de todos</button>
-                </div>
-            </div>
-            <div className='suppliers-container'>
-                <div className='svg-container'>
+            <Box className='suppliers-header' mb="4" mt='50px'>
+                <Heading as="h2" size="lg" mb="4">Empresas</Heading>
+                <Box className='show-data'>
+                    <Button size="md">Ver datos de todos</Button>
+                </Box>
+            </Box>
+            <Box className="card" display="flex" flexDirection={["column", "row"]} alignItems="center" padding="20px" bg="white" borderRadius="md" boxShadow="md">
+                <Box className="image-container" flexShrink="0" mb={["4", "0"]}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="170" height="236" viewBox="0 0 236 236" fill="none">
                         <circle cx="118" cy="118" r="118" fill="url(#paint0_linear_1_33)" />
                         <image href="https://bit.ly/dan-abramov" x="29" y="29" height="178px" width="178px" clipPath="circle(89px at center)" />
@@ -37,18 +38,133 @@ const Participants = () => {
                             </linearGradient>
                         </defs>
                     </svg>
-                </div>
-                <div className='suppliers-info'>
-                    <h4>Nombre de la empresa</h4>
-                    <p>Descripción de la empresa</p>
-                    <p>Tema de ponencias</p>
-                    <p>Asistentes</p>
-                </div>
-            </div>
+                </Box>
+                <Box className="info-container" ml={["0", "20px"]} mb={["4", "0"]}>
+                    <Heading as="h4" size="md">Nombre de la empresa</Heading>
+                    <Text>Descripción de la empresa:</Text>
+                    <Text>Tema de ponencias:</Text>
+                    <Text>Asistentes:</Text>
+                    <button className="info-btn" size="md">+ Info</button>
+                </Box>
+                <Box className="avatar-container" marginLeft="auto" display="flex" alignItems="center">
+                    <AvatarGroup size='sm' max={2}>
+                        <Avatar name='Ryan Florence' src='https://bit.ly/ryan-florence' />
+                        <Avatar name='Segun Adebayo' src='imangen persona' />
+                        <Avatar name='Kent Dodds' src='https://bit.ly/kent-c-dodds' />
+                        <Avatar name='Prosper Otemuyiwa' src='https://bit.ly/prosper-baba' />
+                        <Avatar name='Christian Nwamba' src='https://bit.ly/code-beast' />
+                    </AvatarGroup>
+                </Box>
+            </Box>
+            <Box className="card" display="flex" flexDirection={["column", "row"]} alignItems="center" padding="20px" bg="white" borderRadius="md" boxShadow="md">
+                <Box className="image-container" flexShrink="0" mb={["4", "0"]}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="170" height="236" viewBox="0 0 236 236" fill="none">
+                        <circle cx="118" cy="118" r="118" fill="url(#paint0_linear_1_53)" />
+                        <image href="https://bit.ly/dan-abramov" x="29" y="29" height="178px" width="178px" clipPath="circle(89px at center)" />
+                        <defs>
+                            <linearGradient id="paint0_linear_1_53" x1="37.4603" y1="32.5197" x2="202.574" y2="237.278" gradientUnits="userSpaceOnUse">
+                                <stop stopColor="#FFF48D" />
+                                <stop offset="0.5" stopColor="#DCD279" />
+                                <stop offset="1" stopColor="#FFF7AB" />
+                            </linearGradient>
+                        </defs>
+                    </svg>
+                </Box>
+                <Box className="info-container" ml={["0", "20px"]} mb={["4", "0"]}>
+                    <Heading as="h4" size="md">Nombre de la empresa</Heading>
+                    <Text>Descripción de la empresa:</Text>
+                    <Text>Tema de ponencias:</Text>
+                    <Text>Asistentes:</Text>
+                    <button className="info-btn" size="md">+ Info</button>
+                </Box>
+                <Box className="avatar-container" marginLeft="auto" display="flex" alignItems="center">
+                    <AvatarGroup size='sm' max={2}>
+                        <Avatar name='Ryan Florence' src='https://bit.ly/ryan-florence' />
+                        <Avatar name='Segun Adebayo' src='imangen persona' />
+                        <Avatar name='Kent Dodds' src='https://bit.ly/kent-c-dodds' />
+                        <Avatar name='Prosper Otemuyiwa' src='https://bit.ly/prosper-baba' />
+                        <Avatar name='Christian Nwamba' src='https://bit.ly/code-beast' />
+                    </AvatarGroup>
+                </Box>
+            </Box>
+            <Box className='suppliers-header' mb="4" mt='50px'>
+                <Heading as="h2" size="lg" mb="4">Ponentes</Heading>
+                <Box className='show-data'>
+                    <Button size="md">Ver datos de todos</Button>
+                </Box>
+            </Box>
+            <Box className="card" display="flex" flexDirection={["column", "row"]} alignItems="center" padding="20px" bg="white" borderRadius="md" boxShadow="md">
+                <Box className="image-container" flexShrink="0" mb={["4", "0"]}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="170" height="236" viewBox="0 0 236 236" fill="none">
+                        <circle cx="118" cy="118" r="118" fill="url(#paint0_linear_1_73)" />
+                        <image href="https://bit.ly/dan-abramov" x="29" y="29" height="178px" width="178px" clipPath="circle(89px at center)" />
+                        <defs>
+                            <linearGradient id="paint0_linear_1_73" x1="37.4603" y1="32.5197" x2="202.574" y2="237.278" gradientUnits="userSpaceOnUse">
+                                <stop stopColor="#CBCBCB" />
+                                <stop offset="0.5" stopColor="#E7E7E7" />
+                                <stop offset="1" stopColor="#A4A4A4" />
+                            </linearGradient>
+                        </defs>
+                    </svg>
+                </Box>
+                <Box className="info-container" ml={["0", "20px"]} mb={["4", "0"]}>
+                    <Heading as="h4" size="md">Nombre del ponente</Heading>
+                    <Text>Cargo y empresa:</Text>
+                    <Text>Conocimientos del sector:</Text>
+                    <Text>
+                        <button className='one-2-one-btn'>One2One</button>
+                    </Text>
+                    <button className="info-btn" size="md">+ Info</button>
+                </Box>
+                <Box className="presentation-container" marginLeft="auto"  alignItems="center">
+                    <Box >
+                        <Text fontSize='2xl'>Ponencia</Text>
+                    </Box>
+                    <Box>
+                        <Text>Tema de la ponencia:</Text>
+                        <Text>Hora de la ponencia:</Text>
+                        <Text>Sala de la ponencia:</Text>
+                    </Box>
+                        <Box className='presentation-inf-btn'>
+                        <Button>Ver datos ponencia</Button>
+                        </Box>
+                </Box>
+            </Box>
+            <Box className='suppliers-header' mb="4" mt='50px'>
+                <Heading as="h2" size="lg" mb="4">Asistentes</Heading>
+                <Box className='show-data'>
+                    <Button size="md">Ver datos de todos</Button>
+                </Box>
+            </Box>
+            <Box className="card" display="flex" flexDirection={["column", "row"]} alignItems="center" padding="20px" bg="white" borderRadius="md" boxShadow="md">
+                <Box className="image-container" flexShrink="0" mb={["4", "0"]}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="170" height="236" viewBox="0 0 236 236" fill="none">
+                        <image href="https://bit.ly/dan-abramov" x="29" y="29" height="178px" width="178px" clipPath="circle(89px at center)" />
+                        <defs>
+                            <linearGradient id="paint0_linear_1_73" x1="37.4603" y1="32.5197" x2="202.574" y2="237.278" gradientUnits="userSpaceOnUse">
+                                <stop stopColor="#CBCBCB" />
+                                <stop offset="0.5" stopColor="#E7E7E7" />
+                                <stop offset="1" stopColor="#A4A4A4" />
+                            </linearGradient>
+                        </defs>
+                    </svg>
+                </Box>
+                <Box className="info-container" ml={["0", "20px"]} mb={["4", "0"]}>
+                    <Heading as="h4" size="md">Nombre del asistente</Heading>
+                    <Text>Cargo y empresa:</Text>
+                    <Text>Descripción Asistente:</Text>
+                    <Text>Preferencias:</Text>
+                </Box>
+                <Box className="presentation-container" marginLeft="auto"  alignItems="center">
+                        <Box className='presentation-inf-btn'>
+                        <Button>+ Info</Button>
+                        </Box>
+                </Box>
+            </Box>
 
-        </div>
-
-    )
+        </Box>
+    );
 }
+
 
 export default Participants
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { store } from './app/store.js'
 import {Provider} from 'react-redux'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <ChakraProvider>
     <App />
-  </Provider>,
+  </ChakraProvider>,
+  </Provider>
 )
