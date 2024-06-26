e9645c7ef656907259902aef3b79e7b0de5ab566
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-06-26 14:50:36 +0200

    se crea logica para pintar las empresas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Await, BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./component/Register/Register";
 import CompanyRegister from "./component/CompanyRegister/CompanyRegister";
 import CompanyLogin from "./component/CompanyLogin/CompanyLogin";
-import Login from './component/Login/Login'
+import Login from "./component/Login/Login";
 import Calendar from "./component/Calendar/Calendar";
 import BookingMeetings from "./component/BookingMeetings/BookingMeetings";
 import NotFound from "./component/NotFound/NotFound";
@@ -12,13 +12,14 @@ import Suppliers from "./component/Suppliers/Suppliers";
 function App() {
   return (
     <BrowserRouter>
+      
       <Routes>
         <Route path="/register" element={<Register />} />
-        <Route path='/login' element={<Login/>} />
+        <Route path="/login" element={<Login />} />
         <Route path="/CompanyRegister" element={<CompanyRegister />} />
         <Route path="/CompanyLogin" element={<CompanyLogin />} />
         <Route path="/Calendar" element={<Calendar />} />
-        <Route path="/BookingMeeting" element={<BookingMeetings/>} />
+        <Route path="/BookingMeeting" element={<BookingMeetings />} />
         <Route path="/participants" element={<Participants />} />
         <Route path="/suppliers" element={<Suppliers />} />
         <Route path="*" element={<NotFound />} />
