042f5e56d0164ae61165b23b81fa7c5f031ea4b8
BrunoMalfi <malfi.bruno@gmail.com>
2024-07-03 15:56:05 +0200

    test

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:8080/users/';
+const API_URL = 'http://159.89.21.34:8080/users/';
 
 const register = async (user) => {
     const res = await axios.post(API_URL, user);
